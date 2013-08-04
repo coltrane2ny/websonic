4bce2c205689a6ef8625420a3fca7dee2aeddee5
Yuta Nikaido <coltrane2ny@gmail.com>
2013-08-04 20:59:20 +0900

    delete files do not need version control.

--- server/index.js
+++
@@ -1,9 +0,0 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./request_handlers.js");
-
-var handle = {}
-handle["/play"] = requestHandlers.play;
-handle["/stop"] = requestHandlers.stop;
-
-server.start(router.route, handle);
