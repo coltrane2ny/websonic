9414d513edc22464eb0fb589dca0263b4b94bbb2
Yuta Nikaido <coltrane2ny@gmail.com>
2013-10-22 17:15:29 +0900

    mplayer の再生の仕方を修正

--- player/player.js
+++ player/player.js
@@ -3,18 +3,19 @@ var child_process = require("child_process");
 var Player = function (command, options) {
 	this.cmd = command;
 	this.opts = options;
-	this.child = null;
+	this.child = child_process.spawn(this.cmd, this.opts);
+	var pid = this.child.pid;
+	console.log('player process started. pid: ' + pid);
+	this.child.stdout.on('end', function() {
+		console.log('player process end. pid: ' + pid);
+		this.child = null;
+	});
 
 	Player.prototype.play = function (url) {
 		if (url) {
-			this.opts.push(url);
-			this.child = child_process.spawn(this.cmd, this.opts);
-			var pid = this.child.pid;
-			console.log('player process started. pid: ' + pid);
-			this.child.stdout.on('end', function() {
-				console.log('player process end. pid: ' + pid);
-				this.child = null;
-			});
+			if (this.child) {
+				this.child.stdin.write('loadfile ' + url + '\n');
+			}
 		} else {
 			if (this.child) {
 				this.child.stdin.write('play\n');
